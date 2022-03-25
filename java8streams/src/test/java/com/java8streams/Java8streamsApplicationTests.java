4670884f6ba09d42a8d16607966198daef149f1e
gokulnathcs4u <gokulnathcs4u@gmail.com>
2022-03-25 08:42:37 +0530

    initial commit

---
+++ java8streams/src/test/java/com/java8streams/Java8streamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java8streams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8streamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
