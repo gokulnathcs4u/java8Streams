c029edddee8ff6a4bac93d7bfd28b9a0b316024f
gokulnathcs4u <gokulnathcs4u@gmail.com>
2022-03-27 19:16:11 +0530

    removed logs

--- src/main/java/com/java8streams/controller/NovelController.java
+++ src/main/java/com/java8streams/controller/NovelController.java
@@ -219,7 +219,6 @@ public class NovelController {
 	@CrossOrigin
 	@GetMapping("/history")
 	public ResponseEntity<NovelResponse> getHistory() throws NovelException {
-		System.out.println("called");
 		NovelResponse resp = new NovelResponse();
 		try {
 			resp = novelService.getHistory();
