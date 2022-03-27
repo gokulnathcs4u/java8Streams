c51257b75f15466c293db611d9b86709d056f947
gokulnathcs4u <gokulnathcs4u@gmail.com>
2022-03-27 18:51:30 +0530

    add cross origin

--- src/main/java/com/java8streams/controller/NovelController.java
+++ src/main/java/com/java8streams/controller/NovelController.java
@@ -3,6 +3,7 @@ package com.java8streams.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -214,6 +215,7 @@ public class NovelController {
 		return new ResponseEntity<NovelResponse>(resp, resp.getStatus());
 	}
 
+	@CrossOrigin
 	@GetMapping("/history")
 	public ResponseEntity<NovelResponse> getHistory() throws NovelException {
 		NovelResponse resp = new NovelResponse();
