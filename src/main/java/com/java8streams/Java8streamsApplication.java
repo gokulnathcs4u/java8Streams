2ac8e9a47d5369aafa9ad5c837d85173d4ec9ba1
gokulnathcs4u <gokulnathcs4u@gmail.com>
2022-04-05 13:20:55 +0530

    eureka changes

--- src/main/java/com/java8streams/Java8streamsApplication.java
+++ src/main/java/com/java8streams/Java8streamsApplication.java
@@ -2,8 +2,10 @@ package com.java8streams;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Java8streamsApplication {
 
 	public static void main(String[] args) {
